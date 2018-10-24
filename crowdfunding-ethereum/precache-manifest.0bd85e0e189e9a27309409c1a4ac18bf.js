2a64877cbf9359c4e34b612623cf997eb3c324f8
Mehrad Sadeghi <31504728+mehradsadeghi@users.noreply.github.com>
2018-10-24 23:40:24 +0330

    Minor changes.

--- crowdfunding-ethereum/precache-manifest.0bd85e0e189e9a27309409c1a4ac18bf.js
+++ crowdfunding-ethereum/precache-manifest.0bd85e0e189e9a27309409c1a4ac18bf.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/crowdfunding-ethereum/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/crowdfunding-ethereum/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "32b2a91c6744ec88aff5",
@@ -28,48 +28,48 @@ self.__precacheManifest = [
     "url": "/crowdfunding-ethereum/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/crowdfunding-ethereum/static/media/icons.faff9214.woff"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/crowdfunding-ethereum/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/crowdfunding-ethereum/static/media/icons.faff9214.woff"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/crowdfunding-ethereum/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/crowdfunding-ethereum/static/media/outline-icons.701ae6ab.eot"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/crowdfunding-ethereum/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "32b2a91c6744ec88aff5",
     "url": "/crowdfunding-ethereum/static/js/main.32b2a91c.chunk.js"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/crowdfunding-ethereum/static/media/outline-icons.ad97afd3.ttf"
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/crowdfunding-ethereum/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/crowdfunding-ethereum/static/media/icons.962a1bf3.svg"
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/crowdfunding-ethereum/static/media/outline-icons.ad97afd3.ttf"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/crowdfunding-ethereum/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/crowdfunding-ethereum/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/crowdfunding-ethereum/static/media/outline-icons.ef60a4f6.woff"
-  },
-  {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/crowdfunding-ethereum/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/crowdfunding-ethereum/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/crowdfunding-ethereum/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/crowdfunding-ethereum/static/css/1.0ca09576.chunk.css"
   },
   {
-    "revision": "16e4c02ca22ac1636782a71104c1dfd4",
+    "revision": "f9967f4638d2bca0e24468c808e931c2",
     "url": "/crowdfunding-ethereum/index.html"
   }
 ];
--- crowdfunding-ethereum/service-worker.js
+++ crowdfunding-ethereum/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/crowdfunding-ethereum/precache-manifest.39e7289ab39fa498eede63dee1f93f45.js"
+  "/crowdfunding-ethereum/precache-manifest.0bd85e0e189e9a27309409c1a4ac18bf.js"
 );
 
 workbox.clientsClaim();
